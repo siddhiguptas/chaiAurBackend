8573f868782908caad4d5ea9811ec3592730a6d0
Siddhi905 <siddhigupta811@gmail.com>
2025-03-29 15:05:20 +0530

    Add register controller and bug fixes

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
 
-const app=express()
+const app=express();
 
 app.use(cors({
     origin:process.env.CORS_ORIGIN,
@@ -13,4 +13,11 @@ app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+//routes import
+import userRouter from './routes/user.routes.js'
+
+//routes declaration
+app.use("/api/v1/users",userRouter)
+/*http://localhost:8002/api/v1/users/register*/
 export {app}
--- src/index.js
+++ src/index.js
@@ -2,17 +2,18 @@
 import dotenv from "dotenv"
 
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path:'./env'
 })
 connectDB()
 .then(()=>{
-    app.on("error",()=>{
+    /*app.on("error",()=>{
         console.log("ERR:",error);
         throw error;
-    })
-    app.listen(process.env.PORT||8000,()=>{
+    })*/
+    app.listen(8002,()=>{
         console.log(`Server is running at port : ${process.env.PORT}`)
     })
     
