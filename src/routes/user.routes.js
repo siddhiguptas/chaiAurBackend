b5f9ef05219eca991d9e9b175cafa8dcee5f54ae
Siddhi905 <siddhigupta811@gmail.com>
2025-04-13 17:07:06 +0530

    add new routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import {Router} from "express";
-import { loginUser, logoutUser, registerUser } from "../controllers/user.controllers.js";
+import { changeCurrentPassword, getCurrentUser, getUserChannelProfile, getWatchHistory, loginUser, logoutUser, registerUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage } from "../controllers/user.controllers.js";
 import {upload} from "../middlewares/multer.middlewares.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -27,4 +27,19 @@ router.route("/logout").post(verifyJWT,logoutUser)
 
 router.route("/refresh-token").post(refreshAccessToken)
 
+router.route("//change-password").post(verifyJWT,changeCurrentPassword)
+
+router.route("/current-user").get(verifyJWT,getCurrentUser)
+//patch islie taki kuch chize hi update kare...post se sara ka sara update kr dega
+router.route("/update-account").patch(verifyJWT,updateAccountDetails)
+
+//pehle jwt hona chhahie kyuki pehle user loggedin ho tbhi hme file bheje..second middleware lgayenge upload multer ka
+router.route("/avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar)
+
+router.route("/cover-image").patch(verifyJWT,upload.single("/coverImage"),updateUserCoverImage)
+
+//jb params me se lete ...:k baad milta h
+router.route("/c/:username").get(verifyJWT,getUserChannelProfile)
+
+router.route("/history").get(verifyJWT,getWatchHistory)
 export default router;
